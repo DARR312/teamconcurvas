d45472c2f2164ad78ea5091e7105316d79c03632
Diego Rodríguez <dieguitotoi@gmail.com>
2022-09-11 17:33:56 -0500

    Dieguito

--- team/js/principal.js
+++ team/js/principal.js
@@ -1,5 +1,5 @@
 const urlhost = "http://localhost/wordpress/index.php/controlador" ;//http://localhost/wordpress/index.php/controlador/
-//const urlhost = "https://concurvas.com/team/controlador/" ;sebas
+//const urlhost = "https://concurvas.com/team/controlador/" ;Dieguito
 function formatoPrecio(precio){
     let myFunc = num => Number(num);
     var nuevoPrecio =  Array.from(String(precio), myFunc);
