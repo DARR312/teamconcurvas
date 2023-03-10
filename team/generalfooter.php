58b4d440e622e76e46bc508e15c3418cc0b3d67e
Diego Rodríguez <dieguitotoi@gmail.com>
2023-03-10 14:45:01 -0500

    Poniendo link de insumos

--- team/generalfooter.php
+++ team/generalfooter.php
@@ -65,7 +65,7 @@ $(window).load(function(){
             html =html+ "<div class='col-lg-12 col-md-12 col-sm-2 col-xs-2'>"+
                     "<div class='iconosprincipales col-lg-6 col-md-6 col-sm-6 col-xs-6'>"+
                         "<a id='linkprincipales' href='https://concurvas.com/team/insumos'>"+
-                                "<p class='letra18pt-pc letra3pt-mv nombresiconos'>Insumos</p>"+
+                                "<p class='letra18pt-pc letra3pt-mv nombresiconos'>Proyectos</p>"+
                         "</a></div></div>";
        }
    }
