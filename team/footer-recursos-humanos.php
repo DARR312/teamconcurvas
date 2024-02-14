0c3ccb4d0ac1421bff9a5f6dee126c8c2f86da67
Diego Rodríguez <dieguitotoi@gmail.com>
2024-02-14 12:39:35 -0500

    Arreglando insertar 8

--- team/footer-recursos-humanos.php
+++ team/footer-recursos-humanos.php
@@ -343,7 +343,7 @@ const funcionesRH = () => {
         objeto.valor = 0;
         var salario = prepararjson(objeto);
         // idEmpleado = insertarfila("con_t_empleados",nombre_empleado,id_tipo_empleado,fecha_nacimiento,tipo_identificacion,numero_identificacion,direccion,telefono,correo_electronico,fecha_contratacion,salario,"0");
-        idEmpleado = insertarfila("con_t_empleados",nombre_empleado,id_tipo_empleado,fecha_nacimiento,tipo_identificacion,numero_identificacion,direccion,"0",correo_electronico,fecha_contratacion,"0","0");
+        idEmpleado = insertarfila("con_t_empleados",nombre_empleado,id_tipo_empleado,fecha_nacimiento,tipo_identificacion,numero_identificacion,direccion,correo_electronico,fecha_contratacion,"0","0","0");
         const nuevoEmpleado = $('#nuevoEmpleado');
         const empleadoCreadoOk = $('#empleadoCreadoOk');
 
