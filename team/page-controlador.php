30cfbc2bd26117a13f17c83bcde18419ccbccf14
Diego Rodríguez <dieguitotoi@gmail.com>
2024-03-05 15:49:31 -0500

    Viendo controlador ordenesventa 5

--- team/page-controlador.php
+++ team/page-controlador.php
@@ -910,7 +910,8 @@ function ordenesventajson($valor,$estadoFiltro,$tipoenvio,$datetimepicker_defaul
         
         $consultafinal = "SELECT venta_id,fecha_creada,datos_cliente,pedido,cliente_ok,notas,fecha_entrega,estado,origen,cliente_id FROM con_t_ventas".$where.$est.$tra.$tip.$ent.$ent." ORDER BY venta_id ASC";
         echo $consultafinal;
-        // $ventastodas = $wpdb->get_results($consultafinal, ARRAY_A);
+        $ventastodas = $wpdb->get_results($consultafinal, ARRAY_A);
+        print_r($ventastodas);
         // for ($i=0; $i < 1000; $i++) { 
         //     $where=" WHERE venta_id > ".(1000+($i*1000))." AND venta_id <= ".(2000+($i*1000));
             
