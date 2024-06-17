a70d8ab3b30bcdd5ba4bd311d51e85ff26572b58
Diego Rodríguez <dieguitotoi@gmail.com>
2024-06-17 00:57:54 -0500

    Corrigiendo por que no se ingrresa bien la fecha de inicio

--- team/page-controlador.php
+++ team/page-controlador.php
@@ -1571,8 +1571,8 @@ function insertarfila($tabla,$valor,$valor2,$valor3,$valor4,$valor5,$valor6,$val
     // echo '</br>';
     // echo '</br>';
     // echo '</br>';
-    // echo $datos;
-    $wpdb->query($datos);
+    echo $datos;
+    // $wpdb->query($datos);
     $lastId = $wpdb->get_results( "SELECT MAX(ID) as id FROM ".$tabla."");
     echo json_encode($lastId);
     //echo $tabla."--".$valor."--".$valor2."--".$valor3."--".$valor4."--".$valor5."--".$valor6."--".$valor7."--".$valor8."--".$valor9."--".$valor10."--".$valor11;
