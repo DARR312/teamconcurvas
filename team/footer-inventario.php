5742c06ee2effd22a8fc8f4ce1d58495ba69ef6a
Diego Rodríguez <dieguitotoi@gmail.com>
2024-06-17 00:35:06 -0500

    Quitando botones antiguos

--- team/footer-inventario.php
+++ team/footer-inventario.php
@@ -22,12 +22,12 @@
         if(items[i]==17){
             var segundo = $('#segundo');
             segundo.append("<div class='col-lg-2 col-md-2 col-sm-2 col-xs-12' id='accion17'><button class='botonmodal botonesInventario' type='button' id='crearReferencia'>Referencia nueva </button></div>");
-            segundo.append("<div class='col-lg-2 col-md-2 col-sm-2 col-xs-12' id='accion17'><button class='botonmodal botonesInventario' type='button' id='crearReferenciaVieja'>Referencia nueva - Antiguo </button></div>");
+            // segundo.append("<div class='col-lg-2 col-md-2 col-sm-2 col-xs-12' id='accion17'><button class='botonmodal botonesInventario' type='button' id='crearReferenciaVieja'>Referencia nueva - Antiguo </button></div>");
         }
         if(items[i]==19){
             var segundo = $('#segundo');
-            segundo.append("<div class='col-lg-2 col-md-2 col-sm-2 col-xs-12' id='accion19'><button class='botonmodal botonesInventario' type='button' id='crearCodigos'>Crear códigos</button></div>");
-            // segundo.append("<div class='col-lg-2 col-md-2 col-sm-2 col-xs-12' id='accion19'><button class='botonmodal botonesInventario' type='button' id='fechaslotes'>Fechas de lotes</button></div>");
+            // segundo.append("<div class='col-lg-2 col-md-2 col-sm-2 col-xs-12' id='accion19'><button class='botonmodal botonesInventario' type='button' id='crearCodigos'>Crear códigos</button></div>");
+            segundo.append("<div class='col-lg-2 col-md-2 col-sm-2 col-xs-12' id='accion19'><button class='botonmodal botonesInventario' type='button' id='fechaslotes'>Fechas de lotes</button></div>");
         }
         if(items[i]==20){
             var botonesEscaner = $('#botonesEscaner');
