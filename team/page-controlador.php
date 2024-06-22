81173aa62638e14ba5c89c8d4cd12901da732846
Diego Rodríguez <dieguitotoi@gmail.com>
2024-06-22 12:23:51 -0500

    Error de insertar combinaciones 02

--- team/page-controlador.php
+++ team/page-controlador.php
@@ -1571,7 +1571,7 @@ function insertarfila($tabla,$valor,$valor2,$valor3,$valor4,$valor5,$valor6,$val
     // echo '</br>';
     // echo '</br>';
     // echo '</br>';
-    echo $datos;
+    // echo $datos;
     $wpdb->query($datos);
     $lastId = $wpdb->get_results( "SELECT MAX(ID) as id FROM ".$tabla."");
     echo json_encode($lastId);
