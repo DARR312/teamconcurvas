08ffd3aa0fa61d0cee68de8d6acc5158f2b3074c
Diego Rodríguez <dieguitotoi@gmail.com>
2024-10-10 14:57:22 -0500

    Buscar en ventas plaza no estaba funcionando 5

--- team/footer-ventas-plaza.php
+++ team/footer-ventas-plaza.php
@@ -87,7 +87,7 @@
         $('.primeraFilaDia').css('display', 'block');
         var Etiqueta = $('#BuscarEtiqueta').val();
         if(Etiqueta){
-            var ventasFiltro = obtenerDatajson('ID,fecha_cliente_id,datos_cliente,codigos_prendas,notas,metodos_pago,valor_total','con_t_ventasplaza','Like','codigos_prendas',Etiqueta);
+            var ventasFiltro = obtenerDatajson('*','con_t_ventasplaza','Like','codigos_prendas',Etiqueta);
             var jsonVentasFiltro = JSON.parse(ventasFiltro);
             if(jsonVentasFiltro.length !== 0){
                 $('#primeraFila').after(imprimi(jsonVentasFiltro));
@@ -107,7 +107,7 @@
         $('.primeraFilaDia').css('display', 'block');
 	    var telefono = $('#BuscarTelefono').val();
         if(telefono){
-            var ventasFiltro = obtenerDatajson('ID,cliente_id,datos_cliente,codigos_prendas,notas,metodos_pago,valor_total','con_t_ventasplaza','Like','datos_cliente',telefono);
+            var ventasFiltro = obtenerDatajson('*','con_t_ventasplaza','Like','datos_cliente',telefono);
             var jsonVentasFiltro = JSON.parse(ventasFiltro);
             if(jsonVentasFiltro.length !== 0){
                 $('#primeraFila').after(imprimi(jsonVentasFiltro));
