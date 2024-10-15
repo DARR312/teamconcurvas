c7d80108149a95f80b140f1a857193e3aab13a82
Diego Rodríguez <dieguitotoi@gmail.com>
2024-10-15 18:10:29 -0500

    Nuevo formato de informes 8

--- team/js/inventario.js
+++ team/js/inventario.js
@@ -45,7 +45,7 @@ function inventario(){
     $('.editarValorCambios').on('click', function(){  
         var ids = $(this).attr("name");
         $('#editarValorCambios').attr("name",ids);
-        $('#popup2').fadeIn('slow');         
+        $('#popupDos').fadeIn('slow');         
         $('.popup-overlay').fadeIn('slow');         
         $('.popup-overlay').height($(window).height());
         return false;     
