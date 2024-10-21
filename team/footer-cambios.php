9552a9de13c249cde7fd378c22c3a6c0dd91a18d
Diego Rodríguez <dieguitotoi@gmail.com>
2024-10-21 11:12:52 -0500

    Arreglando cambios

--- team/footer-cambios.php
+++ team/footer-cambios.php
@@ -182,13 +182,19 @@
         var canttrprendas = jsonprendav.length;
         var cantventas = jsonpedidoitemventa[0]['cantidad'];
         if(cantidaddecambjos>0){alert('No se puede hacer el cambio porque la venta ya tiene un cambio asociado');return false;}
+        console.log(canttrprendas);
+        console.log(cantventas);
         var primeraverificación = canttrprendas -cantventas;
         if(primeraverificación>=0){alert('No se puede hacer el cambio porque el cliente aún tiene prendas a su nombre');return false;}        
         var html = "<h1  style='display: none;' class='remover' id='datoscliente' name='"+$('#ventaIdentificacion').val()+"'>"+jsonVentaCliente[0].datos_cliente+"</h1>";
         html = html + "<h1  style='display: none;' class='remover' id='clienteok' name='"+$('#ventaIdentificacion').val()+"'>"+jsonVentaCliente[0].cliente_ok+"</h1>";
         $('#popup').fadeOut('slow');         
         $('#popup3').fadeIn('slow'); 
-        html = html+"<div class='col-lg-3 col-md-3 col-sm-3 col-xs-3 remover'><button class='botonmodal remover botoncargar' id='botonCargacambios' >Cargar</button></div><div class='form-group pmd-textfield pmd-textfield-floating-label col-lg-8 col-md-8 col-sm-8 col-xs-8 remover'><label for='cantidad6' class='control-label letra18pt-pc'> Valor de envío a pagar por el cliente</label><input class='form-control' type='number' id='costosEnvio' name='costosEnvio' min='1'><span class='pmd-textfield-focused'></span></div>";
+        html = html+"<div class='col-lg-3 col-md-3 col-sm-3 col-xs-3 remover'>";
+        html +="<button class='botonmodal remover botoncargar' id='botonCargacambios' >Cargar</button></div>";
+        html +="<div class='form-group pmd-textfield pmd-textfield-floating-label col-lg-8 col-md-8 col-sm-8 col-xs-8 remover'>";
+        html +="<label for='cantidad6' class='control-label letra18pt-pc'> Valor de envío a pagar por el cliente</label>";
+        html +="<input class='form-control' type='number' id='costosEnvio' name='costosEnvio' min='1'><span class='pmd-textfield-focused'></span></div>";
         $("#formularioPedido").after(html);
         html = "<option value='NA'>NA</option>";
         var disponibl = disponibles();
