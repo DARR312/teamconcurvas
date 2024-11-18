70fc0b258d2eb18d82d72ff0077de6f1ab56aa3b
Diego Rodríguez <dieguitotoi@gmail.com>
2024-11-18 10:03:41 -0500

    Creando ajustar prendas 2

--- team/footer-inventario.php
+++ team/footer-inventario.php
@@ -104,7 +104,9 @@
         $('#madrugonDiv').css('display', 'none'); 
         $('#fechaslotesdiv').css('display', 'none');
         $('#ajustarManual').css('display', 'block');
-       
+        let listado = obtenerDatajson("estado","con_t_trprendas","variasfilasunicas","0","0");
+        let listadoEstados =  JSON.parse(listado);    
+        console.log(listadoEstados);
         return false;     
     }); 
     $('#crearReferenciaVieja').on('click', function(){   
